21054a94eda9e3e81d786306cfe1d83d34ed597c
Mykhailo Pyshnyi <misha709@gmail.com>
2026-02-01 16:57:54 +0200

    Lessons/11 docker compose (#7)
    * Init multi-container-todo
    
    * Update app to load balance traffic

---
+++ 11_docker_compose/multi-container-todo/api/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Api;
+using Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<Todo> Todos { get; set; }
+}
